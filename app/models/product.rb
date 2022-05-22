5bf4cb21922e803814a16190593d89036837d82a
etherell <kostyaetherell@gmail.com>
2022-05-22 10:56:22 +0300

    create new stripe price and assign to product when product price is updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,4 +20,12 @@ class Product < ApplicationRecord
     # Adds stripe product id and stripe_price_id to DB
     update(stripe_product_id: product.id, stripe_price_id: price.id)
   end
+
+  # Updates price on Stripe when it updated in DB
+  after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+
+  def create_and_assign_new_stripe_price
+    price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: 'usd')
+    update(stripe_price_id: price.id)
+  end
 end
